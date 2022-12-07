6be666705fc341e44725db3bc4d0d66746357ea8
HAARPERFT <rajashawal246@gmail.com>
2022-12-07 16:08:58 +0800

    update ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
           
         </div> <!-- container //  -->
         </nav> <!-- header-top-light.// -->
-        
+        <br>
         <section class="header-main border-bottom">
           <div class="container">
         <div class="row align-items-center">
@@ -58,19 +58,7 @@
                 </div>
             </form> <!-- search-wrap .end// -->
           </div> <!-- col.// -->
-          <div class="col-lg-4 col-sm-6 col-12">
-            <div class="widgets-wrap float-md-right">
-              <div class="widget-header  mr-3">
-                <a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
-                <span class="badge badge-pill badge-danger notify">0</span>
-              </div>
-              <div class="widget-header icontext">
-                <a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-user"></i></a>
-                <div class="text">
-                  <span class="text-muted">Welcome!</span>
-                  <div> 
-                    <a href="{{ route('login') }}">Log In</a> |  
-                    <a href="{{ route('register') }}"> Register</a>
+          <br><br>
                   </div>
                 </div>
               </div>
