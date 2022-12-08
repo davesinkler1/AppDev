689348d4d64152bda35de6b1d62bc10f28b62995
HAARPERFT <rajashawal246@gmail.com>
2022-12-08 17:54:35 +0800

    update ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,18 @@
             </div> <!-- container .// -->
           </nav>
           
+          <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </div>
+        
           </header> <!-- section-header.// -->
 
         <!-- ========================= LOGIN FORM ========================= -->
